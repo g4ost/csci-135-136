45447fce6d468a435f78322bdaa99afc84953288
g4ost <37353294+g4ost@users.noreply.github.com>
2018-08-28 15:37:55 -0400

    Update smaller3.cpp

--- smaller3.cpp
+++ smaller3.cpp
@@ -7,6 +7,13 @@ Assignment: Lab 1
 Write a program smaller3.cpp that asks the user to input three integer numbers, and prints out the smallest of the three.
 
 (Hint: There are many possible solutions here. One possible strategy: Given numbers x, y, and z, you can first compare x and y, take whichever is smaller and compare it with z.)
+uifgwbgnerhtge
+h
+
+ther
+htr
+htr
+
 */
 
 #include <iostream>
