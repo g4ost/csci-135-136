4513ce6df89005e5115b448ca3e041c959728c72
g4ost <37353294+g4ost@users.noreply.github.com>
2018-08-30 09:14:09 -0400

    Update month.cpp

--- month.cpp
+++ month.cpp
@@ -25,17 +25,135 @@ int main()
 
 	cin >> year;
   
-  	cout << "Enter month" << endl;
+  	cout << "Enter month"cout << endl;
 
 	int month = 0;
 
 	cin >> month;
-  
-  
-	switch (year)
-  	{
-		case (year % 4 != 0 & month == 2): printf("29 days")
-			break;
-		case (year % 4 != 0 & month % 2 != 0): printf("29 days")
+  	
+	int jan  = 31
+	int feb1 = 28
+	int feb2 = 29
+	int mar  = 31
+	int apr  = 30
+	int may  = 31
+	int jun  = 30
+	int jul  = 31
+	int aug  = 31
+	int sep  = 30
+	int oct  = 31
+	int nov  = 30
+	int dec  = 31
 	
-}
+	if (year % 4 != 0)
+  	{
+    		if (month = 1)
+			cout << jan << " days" << endl;
+		else if (month = 2)
+			cout << feb1 << endl;
+		else if (month = 3)
+			cout << mar << endl;
+		else if (month = 4)
+			cout << apr << endl;
+		else if (month = 5)
+			cout << may << endl;
+		else if (month = 6)
+			cout << jun << endl;
+		else if (month = 7)
+			cout << jul << endl;
+		else if (month = 8)
+			cout << aug << endl;
+		else if (month = 9)
+			cout << sep << endl;
+		else if (month = 10)
+			cout << oct << endl;
+		else if (month = 11)
+			cout << nov << endl;
+		else
+			cout << dec << endl;	
+  	}
+	
+	else if (year % 100 != 0)
+  	{
+    		if (month = 1)
+			cout << jan << " days" << endl;
+		else if (month = 2)
+			cout << feb2 << endl;
+		else if (month = 3)
+			cout << mar << endl;
+		else if (month = 4)
+			cout << apr << endl;
+		else if (month = 5)
+			cout << may << endl;
+		else if (month = 6)
+			cout << jun << endl;
+		else if (month = 7)
+			cout << jul << endl;
+		else if (month = 8)
+			cout << aug << endl;
+		else if (month = 9)
+			cout << sep << endl;
+		else if (month = 10)
+			cout << oct << endl;
+		else if (month = 11)
+			cout << nov << endl;
+		else
+			cout << dec << endl;
+	}
+		
+		else if (year % 400 != 0)
+  	{
+    		if (month = 1)
+			cout << jan << " days" << endl;
+		else if (month = 2)
+			cout << feb1 << endl;
+		else if (month = 3)
+			cout << mar << endl;
+		else if (month = 4)
+			cout << apr << endl;
+		else if (month = 5)
+			cout << may << endl;
+		else if (month = 6)
+			cout << jun << endl;
+		else if (month = 7)
+			cout << jul << endl;
+		else if (month = 8)
+			cout << aug << endl;
+		else if (month = 9)
+			cout << sep << endl;
+		else if (month = 10)
+			cout << oct << endl;
+		else if (month = 11)
+			cout << nov << endl;
+		else
+			cout << dec << endl;
+  	}
+	else
+  	{
+    		if (month = 1)
+			cout << jan << " days" << endl;
+		else if (month = 2)
+			cout << feb2 << endl;
+		else if (month = 3)
+			cout << mar << endl;
+		else if (month = 4)
+			cout << apr << endl;
+		else if (month = 5)
+			cout << may << endl;
+		else if (month = 6)
+			cout << jun << endl;
+		else if (month = 7)
+			cout << jul << endl;
+		else if (month = 8)
+			cout << aug << endl;
+		else if (month = 9)
+			cout << sep << endl;
+		else if (month = 10)
+			cout << oct << endl;
+		else if (month = 11)
+			cout << nov << endl;
+		else
+			cout << dec << endl;
+  	}
+
+	}
