deb706333217918f2ffa9ffe132b60fade77ac41
g4ost <37353294+g4ost@users.noreply.github.com>
2018-09-06 14:51:40 -0400

    Update valid.cpp

--- valid.cpp
+++ valid.cpp
@@ -1,1 +1,28 @@
+/*
+Author: Stanley Gao
+Course: CSCI-136
+Instructor: Gwenael Gatto
+Assignment: Lab 2
+Write a program valid.cpp, 
+which asks the user to input an integer in the range 0 < n < 100. 
+If the number is out of range, 
+the program should keep asking to re-enter until a valid number is input.
+arhwaevxtdcfyvuhj
+xdxtcfyvubidxfcgvhbjn
+fsdxgchvjbkjdxgfchvjbkjn
+xdgfchvbjnfdxgfcvjhkbnml
+rzdxgfchvjbknldxgfchvjbkjn
+dxgfchvjbknlkgxfchvjbknlm
+fxdgfchvjbknlfdxgchvb
+xgdfchvjbknlgxdfchvjbk5
 
+After a valid value is obtained, print this number n squared.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  
