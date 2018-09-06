7c5932ec3a146b37a75d5ff38ff094b89c80f7bb
g4ost <37353294+g4ost@users.noreply.github.com>
2018-09-06 15:32:56 -0400

    Create print-interval.cpp

---
+++ print-interval.cpp
@@ -0,0 +1,42 @@
+/*
+Author: Stanley Gao
+Course: CSCI-136
+Instructor: Gwenael Gatto
+Assignment: Lab 2
+Write a program print-interval.cpp 
+that asks the user to input 
+two integers L and U (representing the lower and upper limits of the interval), 
+and print out all integers in the range L ≤ i < U separated by spaces.
+Notice that we include the lower limit, but exclude the upper limit.
+ki7umynthbgrvfecdwxs
+btgrvfecdwxsnyhtbgrvfd
+nhtbgrvfcedmjnyhbtgvrfced
+nuybtgvrfcednyhbtgvr
+nyhbtgvrfcednuybtgv
+munyhbtvrcmujnyhbtgvr
+jnyhbtgvrfcemkujnyhbtg
+mujnyhbtgvrfcnujy
+hbtnujyhbtgvrf
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int L = 0;
+    int U = 0;
+
+    cout << "enter L" << endl;
+    cin >> L;
+    
+    cout << "enter U" << endl;
+    cin >> U;
+
+    for (i = L, i < U, i++);
+    {
+        cout << i << endl;
+    }
+
+
+}
