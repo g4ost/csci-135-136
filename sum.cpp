e32358049176d8597c11b2821b30c5aab5b7d964
g4ost <37353294+g4ost@users.noreply.github.com>
2018-09-11 17:12:54 -0400

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -2,6 +2,10 @@
 //Csci 135
 //Project 1
 //Task A
+//3qrhuvibhj 3rohivjbk
+//3orfb3qrowibjvkr e/3u
+//4uhfirbhjv ohi3ugjkrv/3grefvg4rev/g53revfwrev/rwevf
+//rev3qrwvds
 #include <iostream>
 using namespace std;
 
