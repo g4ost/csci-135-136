237f51bfba551fe686c888b626bd38e7fed108da
g4ost <37353294+g4ost@users.noreply.github.com>
2018-09-11 17:09:17 -0400

    Update and rename project 1 135 to sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,20 @@
+//Stanley Gao
+//Csci 135
+//Project 1
+//Task A
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int s = 0;
+    int n;
+    while(cin >> n) 
+    {
+        int a = n;
+        s += a;
+    }
+    cout << s;
+
+    return 0;
+}
